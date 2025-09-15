202e686913003cd9e28826e222a265dfa131fae2
MD ABUBAKAR <abubakar.jawed@gmail.com>
2025-09-15 13:36:08 +0530

    added more trackers

--- seedr-server/src/services/torrentManager.js
+++ seedr-server/src/services/torrentManager.js
@@ -1,159 +1,8 @@
-// // src/services/torrentManager.js (CJS compatible with ESM webtorrent)
-// const prettyBytes = require('pretty-bytes');
-// const path = require('path');
-// const { logger } = require('../utils/logger');
-
-// const ROOT = process.env.ROOT || './src/storage/library';
-
-// let WebTorrentMod;   // will hold the ESM default export
-// let client;          // singleton client instance
-
-// async function getClient() {
-//   if (!WebTorrentMod) {
-//     // Dynamically import the ESM module at runtime
-//     WebTorrentMod = (await import('webtorrent')).default;
-//   }
-//   if (!client) {
-//     client = new WebTorrentMod({
-//       // tune opts if needed
-//     });
-//     client.on('error', (e) => logger.error('WebTorrent error:', e.message));
-//   }
-//   return client;
-// }
-
-// function toSummary(t) {
-//   return {
-//     id: t.infoHash,
-//     name: t.name,
-//     progress: Number((t.progress * 100).toFixed(2)),
-//     downloaded: prettyBytes(t.downloaded),
-//     length: prettyBytes(t.length || 0),
-//     downloadSpeed: prettyBytes(t.downloadSpeed) + '/s',
-//     uploadSpeed: prettyBytes(t.uploadSpeed) + '/s',
-//     numPeers: t.numPeers,
-//     files: t.files.map((f, i) => ({
-//       index: i,
-//       name: f.name,
-//       path: f.path,
-//       length: f.length
-//     })),
-//     done: t.done
-//   };
-// }
-
-// // async function addMagnet(magnet) {
-// //   const c = await getClient();
-// //   return new Promise((resolve, reject) => {
-// //     const t = c.add(magnet, { path: path.resolve(ROOT) }, (torrent) => {
-// //       torrent.on('ready', () => resolve(toSummary(torrent)));
-// //     });
-// //     t.on('error', reject);
-// //   });
-// // }
-
-
-// // async function addMagnet(magnet) {
-// //   const c = await getClient();
-// //   return new Promise((resolve, reject) => {
-// //     const t = c.add(
-// //       magnet,
-// //       { path: path.resolve(ROOT) },
-// //       (torrent) => {
-// //         // ====== LOGGING ======
-// //         torrent.on('infoHash', () =>
-// //           console.log('got infoHash', torrent.infoHash)
-// //         );
-// //         torrent.on('metadata', () =>
-// //           console.log('got metadata for', torrent.name)
-// //         );
-// //         torrent.on('ready', () =>
-// //           console.log('torrent ready', torrent.name)
-// //         );
-// //         torrent.on('error', (err) =>
-// //           console.error('torrent error:', err)
-// //         );
-// //         torrent.on('noPeers', (type) =>
-// //           console.log('no peers from', type)
-// //         );
-// //         // ======================
-
-// //         torrent.on('ready', () => resolve(toSummary(torrent)));
-// //       }
-// //     );
-// //     t.on('error', reject);
-// //   });
-// // }
-
-
-// const trackers = [
-//   'udp://tracker.opentrackr.org:1337/announce',
-//   'udp://tracker.torrent.eu.org:451/announce',
-//   'udp://open.stealth.si:80/announce',
-//   'udp://exodus.desync.com:6969/announce',
-//   'http://tracker.opentrackr.org:1337/announce',
-//   'http://open.tracker.cl:1337/announce'
-// ];
-
-// async function addMagnet(magnet) {
-//   const c = await getClient();
-//   return new Promise((resolve, reject) => {
-//     const t = c.add(
-//       magnet,
-//       { path: path.resolve(ROOT), announce: trackers },
-//       (torrent) => {
-//         // logging to see lifecycle
-//         torrent.on('infoHash', () => console.log('got infoHash', torrent.infoHash));
-//         torrent.on('metadata', () => console.log('got metadata for', torrent.name));
-//         torrent.on('ready', () => console.log('torrent ready', torrent.name));
-//         torrent.on('error', (err) => console.error('torrent error:', err));
-//         torrent.on('noPeers', (type) => console.log('no peers from', type));
-
-//         torrent.on('ready', () => resolve(toSummary(torrent)));
-//       }
-//     );
-//     t.on('error', reject);
-//   });
-// }
-
-
-
-// async function getTorrent(infoHash) {
-//   const c = await getClient();
-//   return c.get(infoHash) || null;
-// }
-
-// async function listTorrents() {
-//   const c = await getClient();
-//   return c.torrents.map(toSummary);
-// }
-
-// async function removeTorrent(infoHash) {
-//   const c = await getClient();
-//   const t = c.get(infoHash);
-//   if (!t) return false;
-//   return new Promise((resolve, reject) => {
-//     c.remove(infoHash, { destroyStore: false }, (err) => {
-//       if (err) return reject(err);
-//       resolve(true);
-//     });
-//   });
-// }
-
-// module.exports = {
-//   addMagnet,
-//   getTorrent,
-//   listTorrents,
-//   removeTorrent
-// };
-
-
-
-
 
 // src/services/torrentManager.js (CJS compatible, no pretty-bytes)
 const path = require('path');
 const { logger } = require('../utils/logger');
+const { getTrackers } = require('../utils/trackers');
 
 const ROOT = process.env.ROOT || './src/storage/library';
 
@@ -220,23 +69,60 @@ const trackers = [
   'http://open.tracker.cl:1337/announce'
 ];
 
+// async function addMagnet(magnet) {
+//   const c = await getClient();
+//   return new Promise((resolve, reject) => {
+//     const t = c.add(
+//       magnet,
+//       { path: path.resolve(ROOT), announce: trackers },
+//       (torrent) => {
+//         // helpful logging
+//         torrent.on('infoHash', () => console.log('got infoHash', torrent.infoHash));
+//         torrent.on('metadata', () => console.log('got metadata for', torrent.name));
+//         torrent.on('ready', () => console.log('torrent ready', torrent.name));
+//         torrent.on('error', (err) => console.error('torrent error:', err));
+//         torrent.on('noPeers', (type) => console.log('no peers from', type));
+
+//         torrent.on('ready', () => resolve(toSummary(torrent)));
+//       }
+//     );
+//     t.on('error', reject);
+//   });
+// }
+
+
 async function addMagnet(magnet) {
   const c = await getClient();
+  const announce = getTrackers();
   return new Promise((resolve, reject) => {
     const t = c.add(
       magnet,
-      { path: path.resolve(ROOT), announce: trackers },
+      { path: path.resolve(ROOT), announce },
       (torrent) => {
-        // helpful logging
+        // logging
         torrent.on('infoHash', () => console.log('got infoHash', torrent.infoHash));
         torrent.on('metadata', () => console.log('got metadata for', torrent.name));
         torrent.on('ready', () => console.log('torrent ready', torrent.name));
         torrent.on('error', (err) => console.error('torrent error:', err));
         torrent.on('noPeers', (type) => console.log('no peers from', type));
 
+        // ⛔ Stop seeding as soon as download finishes (keep files)
+        torrent.on('done', () => {
+          console.log('download complete, removing torrent to stop seeding:', torrent.infoHash);
+          c.remove(torrent.infoHash, { destroyStore: false }, (err) => {
+            if (err) {
+              console.error('error removing torrent:', err);
+            } else {
+              console.log('torrent removed:', torrent.infoHash);
+            }
+          });
+        });
+
+        // resolve summary when torrent is ready (metadata available)
         torrent.on('ready', () => resolve(toSummary(torrent)));
       }
     );
+
     t.on('error', reject);
   });
 }
