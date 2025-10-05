5ef0eb6b67481ad0e4c538da14c10ab5e2b3162e
abujawed11 <abubakar.jawed@gmail.com>
2025-10-05 12:47:44 +0530

    corrected url

--- seedr-web/src/api.js
+++ seedr-web/src/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 // 👇 Change this to your server domain or IP
-const API_BASE = import.meta.env.VITE_API_BASE || "http://localhost:5000";
+// const API_BASE = import.meta.env.VITE_API_BASE || "http://localhost:5000";
+const API_BASE = import.meta.env.VITE_API_BASE || "";
 
 const api = axios.create({
   baseURL: `${API_BASE}/api`,
